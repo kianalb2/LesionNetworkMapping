a1078f082aeac2839377e162f00aacde67bb0da0
Kiana Bunnell <55462155+kianalb2@users.noreply.github.com>
2026-03-19 14:15:51 -0500

    Create BatchFSLMeants.sh

---
+++ code/CopyScripts/Step1/BatchFSLMeants.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --time=5:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=100GB   # memory per CPU core
+#SBATCH -J "average_time_series_extraction"   # job name
+EXPORTHERE
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+cd ${home_folder}/LNM_Home_${analysis_name}/cache/scripts/COCKPIT/Step1
+
+bash FSLMeantsParallel.sh $1 
+
+
